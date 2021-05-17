b8a767cc297a8b6693cea80d37655798f7570744
Priya Sadh <71939935+psadh99@users.noreply.github.com>
2021-05-17 21:10:44 +0400

    Update academy (1).py

--- academy (1).py
+++ academy (1).py
@@ -5,34 +5,21 @@ import cv2
 # Enter sequential code!
 
 prev_error = 0
-accum_error = 0
 
 while True:
     # Enter iterative code!
     #get the image and its dimensions
     image = HAL.getImage()
+    
     #convert to hsv scale and define thresholds for red color
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    lower_thresh1 = np.array([0, 60, 20])
-    upper_thresh1 = np.array([7, 100, 60])
+    lower_thresh = np.array([0, 0, 0])
+    upper_thresh = np.array([1, 1, 360])
     
-    lower_thresh2 = np.array([350, 60, 40])
-    upper_thresh2 = np.array([360, 90, 60])
-    
-    mask1 = cv2.inRange(hsv, lower_thresh1, upper_thresh2)
-    mask2 = cv2.inRange(hsv, lower_thresh2, upper_thresh2)
-    
-    print("Running")
-    #apply a mask
-    mask = mask1 + mask2
-    
-    mask = cv2.bitwise_not(mask)
-    #mask[0:search_top, 0:w] = 0
-    #mask[search_bot:h, 0:w] = 0
+    mask = cv2.inRange(hsv, lower_thresh, upper_thresh)
+    mask = cv2.bitwise_not(mask1)
     
     h, w, d = image.shape
-    search_top = 3 * h / 4
-    search_bot = search_top * 20
     
     #calculate centre of line detected using moments
     M = cv2.moments(mask)
